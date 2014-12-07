3d3c525488ced7988a617e9221b89e91a1e221d8
Geremia Taglialatela <tagliala.dev@gmail.com>
2014-12-07 18:21:11 +0100

    updated config files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'localhost' }
 
-  # Randomize the order test cases are executed
+  # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
   # Print deprecation notices to the stderr.
