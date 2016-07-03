f49b66a39bf1370dd4487d0e96145448122f466a
Geremia Taglialatela <tagliala.dev@gmail.com>
2016-07-03 15:58:45 +0200

    Change “Hello world” to “Hello World”

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Welcome' do
   context 'Index' do
-    it "has 'Hello world' in title" do
+    it "has 'Hello World' in title" do
       visit root_path
 
       expect(page).to have_title I18n.t('hello')
